89fc8853241ec2c47638b34824d6b3929b33d33f
massimodeagro <47501397+massimodeagro@users.noreply.github.com>
2019-10-14 16:10:11 +0200

    Added comments

--- OS/lists.py
+++ OS/lists.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-SkinnerBox standalone version
+SPiDbox standalone version
 
 subject list and other saved informations
 
@@ -9,9 +8,9 @@ subject list and other saved informations
 
 from os import listdir
 
-def loadsubjs():
+def loadsubjs(): #simply, the formula reads the folders in /home/subjs/. This is also where videos and csvs are saved
     subjs = listdir('subjs/')
-    subjs = ['back']+subjs
+    subjs = ['back']+subjs #the word "back" is added to the subject list, in order for the main menu to be able to go back
     return subjs
     
-subjs = loadsubjs()
+subjs = loadsubjs() #also, create the list
