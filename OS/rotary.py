c8be3cb049fb568488289010e1339591156b7d8e
massimodeagro <47501397+massimodeagro@users.noreply.github.com>
2019-10-14 16:26:10 +0200

    Added comments

--- OS/rotary.py
+++ OS/rotary.py
@@ -1,7 +1,17 @@
+
+"""
+SPiDbox
+
+@author: Massimo De Agrò
+"""
+
+#modules
 from RPi import GPIO
 
-
-def rotation(cl,dt,outold):
+#this formula read the state of both cl and dt, as well as starting from the old reading of those.
+#a movement is detected based on the current state and the new state. If those two are different, it means that the rotor moved.
+#Then, depending on the position, a clockwise (1) or counterclockwise (-1) rotation can be detected.
+def rotation(cl,dt,outold): 
     a=GPIO.input(cl)
     b=GPIO.input(dt)
     out = str(a)+str(b)
@@ -30,12 +40,12 @@ def rotation(cl,dt,outold):
         outold = out
     return rotating,outold
 
-
+#this formula catches clicks
 def button(sw):
     out = not GPIO.input(sw)
     return out
     
-
+#hthis formula loops all together, continously catching cl and dt state, as well as checking if the button has been clicked
 def rothread(cl,dt,sw):
     outold = '00'
     casechange = 0
