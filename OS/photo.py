4524df550f21c31c2b093050638d98af780dc5f0
massimodeagro <47501397+massimodeagro@users.noreply.github.com>
2019-10-14 15:48:48 +0200

    Added comments

--- OS/photo.py
+++ OS/photo.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 """
-SkinnerBox standalone version
+SPiDbox standalone version
 
 module of the photoresistor reading
 
 V0.1 of photoresistor voltage reading after the comparator working
-This is called by main program and single tests
+This is called by main program and single tests. for a complete explanation of the system see the paper
 
 @author: Massimo De Agrò
 """
 
-#%% package load
-
 #%%general variables
 
 filt_c = 0.8   #filtering constant. how smoothed the raw data get
-tresh_c = 0.11 #treshold constant. how much lower of the median average is the treshold, expressed in proportiom
+tresh_c = 0.11 #treshold constant. how much lower of the median average is the treshold, expressed in proportion
 Eavg_c = 0.03  #averaging constant
 
-def binphoto (adc, channel, filt_c,tresh_c,adc_filt,adc_Eavg):
+def binphoto (adc, channel, filt_c,tresh_c,adc_filt,adc_Eavg): #calculate if the sensor has been covered, comparing treshold, raw reading, moving average
 
     adc_raw = adc.read_adc(channel, gain=1)
     
@@ -32,6 +28,6 @@ def binphoto (adc, channel, filt_c,tresh_c,adc_filt,adc_Eavg):
   
     return covered, adc_filt
 
-def Eavgcalc(adc_filt, adc_Eavg, Eavg_c):
+def Eavgcalc(adc_filt, adc_Eavg, Eavg_c): #moving average formula
     adc_Eavg = (Eavg_c * adc_filt) + (( 1 - Eavg_c ) * adc_Eavg)
     return adc_Eavg
