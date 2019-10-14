365c2c5c9de13450c6dcee7ef9178f6184a40714
massimodeagro <47501397+massimodeagro@users.noreply.github.com>
2019-10-14 15:54:05 +0200

    Added  comments

--- OS/pin.py
+++ OS/pin.py
@@ -1,52 +1,56 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Nov 22 19:14:13 2017
+SPiDbox 
 
-@author: frogmax
+this file contains the definition for all the pins of the system
+
+@author: Massimo De Agrò
 """
 
-import RPi.GPIO as GPIO
-dc = 37
-    
-stp = 15
-clk = 16
+import RPi.GPIO as GPIO #module that control the pins
 
-#
+dc = 37 #DC peristaltic pump pin
+ 
+#Stepper peristaltic pump
+stp = 15 #pin that drives motor steps
+clk = 16 #pin for direction
+
+# values to make the stepper motor do microsteps. These are generally turned off
 M1 = 40
 M2 = 38
 M3 = 36
 
-# rotary
-cl = 8
-dt = 10
-sw = 12
+# rotary encoder
+cl = 8 #clock pin of the rotary
+dt = 10 #dt pin of the rotary
+sw = 12 #switch. defined also as ok for clarity in some formulas
 ok = 12
 
-led = 35
+led = 35 # not used anymore
 
 #photoresistors channels on i2c
 photoL = 0
 photoR = 1
 
-GPIO.setmode(GPIO.BOARD) #set board
+GPIO.setmode(GPIO.BOARD) #set board mode
 
-#set buttons
+#set rotor pins as input
 GPIO.setup(cl,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(sw,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(led,GPIO.OUT)
-# set dc motors
+GPIO.setup(led,GPIO.OUT) #not used anymore
+
+# set dc motor as output and then put it low
 GPIO.setup(dc,GPIO.OUT)
 GPIO.output(dc,GPIO.LOW)
 
-#set peristaltic
+#set peristaltic as out and then put it low
 GPIO.setup(stp,GPIO.OUT)
 GPIO.output(stp,GPIO.LOW)
 
 GPIO.setup(clk,GPIO.OUT)
 GPIO.output(clk,GPIO.LOW)
 
-#setphoto
+#old photoresistor method. I keep it for transparency
 #GPIO.setup(photoL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.setup(photoR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
