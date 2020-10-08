390a00fcd8c0aef4456e1e51540378dadf936026
massimodeagro <47501397+massimodeagro@users.noreply.github.com>
2020-10-08 17:36:15 +0200

    Update drop.py
    Simplified, to just work for 1/6 of turns, to avoid overlapping of similar systems that count the exact number of steps

--- OS/drop.py
+++ OS/drop.py
@@ -28,45 +28,21 @@ def stepmove(step_pin,dir_pin,reverse,steps,position): #move the stepper motor f
         GPIO.output(step_pin,GPIO.LOW)
         time.sleep(0.01)
         position+=1
-        #this catches the points in which the pumps sucks instead of pushing, fastly skipping 14 steps
-        if position==19 or position==52 or position==119 or position==152: 
-            for i in range(14):
-                GPIO.output(step_pin,GPIO.HIGH)
-                time.sleep(0.01)
-                GPIO.output(step_pin,GPIO.LOW)
-                time.sleep(0.01)
-                position+=1
-        if position==85 or position==185: #or 15 steps, since 100/3 is 33+33+34
-            for i in range(15):
-                GPIO.output(step_pin,GPIO.HIGH)
-                time.sleep(0.01)
-                GPIO.output(step_pin,GPIO.LOW)
-                time.sleep(0.01)
-                position+=1
+        #if you are at third 3 of 6 or 6 of 6, do another step, as 200/6 is not a whole number
+        if position==99 or position==199: 
+            GPIO.output(step_pin,GPIO.HIGH)
+            time.sleep(0.01)
+            GPIO.output(step_pin,GPIO.LOW)
+            time.sleep(0.01)
+            position+=1
         if position==200:
             position=0
 
 def postrack(steps, position): #this formula does the same exact steps without moving the motor. It is used to count the steps
     for i in range(steps):
         position+=1
-        if position==19 or position==52 or position==119 or position==152:
-            for i in range(14):
-                position+=1
-        if position==85 or position==185:
-            for i in range(15):
-                position+=1
+        if position==99 or position==199:
+            position+=1
         if position==200:
             position=0
     return position
-
-def onedrop(step_pin): #formula already specified to give one drop. However, it depends on the size of the holder, tubes, etc.
-    for i in range(85):
-        GPIO.output(step_pin,GPIO.HIGH)
-        time.sleep(0.01)
-        GPIO.output(step_pin,GPIO.LOW)
-        time.sleep(0.01)
-    for i in range(15):
-        GPIO.output(step_pin,GPIO.HIGH)
-        time.sleep(0.05)
-        GPIO.output(step_pin,GPIO.LOW)
-        time.sleep(0.05)
